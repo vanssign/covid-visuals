b0a4b909a0f1c32460c445a7f1c45b5bd6695270
vanssign <thindvansh1999@gmail.com>
2020-09-07 09:37:22 +0530

    Updates

---
+++ precache-manifest.afa4e8de5dffc212789b161d37ac14d5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7e034df5be563cb76060d48e3f3abf",
+    "url": "/covid-visuals/index.html"
+  },
+  {
+    "revision": "db230be881c0ef98f636",
+    "url": "/covid-visuals/static/css/2.6b6eb0d3.chunk.css"
+  },
+  {
+    "revision": "bede58a1e81ad9b38174",
+    "url": "/covid-visuals/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "db230be881c0ef98f636",
+    "url": "/covid-visuals/static/js/2.26dfc694.chunk.js"
+  },
+  {
+    "revision": "26eaf80254b52f2ebcf3542565f78969",
+    "url": "/covid-visuals/static/js/2.26dfc694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bede58a1e81ad9b38174",
+    "url": "/covid-visuals/static/js/main.568d5bf2.chunk.js"
+  },
+  {
+    "revision": "703fcccd105045416a0e",
+    "url": "/covid-visuals/static/js/runtime-main.17be3502.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/covid-visuals/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/covid-visuals/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/covid-visuals/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/covid-visuals/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/covid-visuals/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
